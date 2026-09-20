d02b2af2e2237a721d21762f8e525cb84e7af6a4
santoshgouda679 <santoshgoudag2@gmail.com>
2026-09-20 13:10:28 +0530

    Remove Employee Management System files from this repo

--- employee_management_system/src/main/java/com/employee_management_system/exception/InvalidOtpException.java
+++
@@ -1,7 +0,0 @@
-package com.employee_management_system.exception;
-
-public class InvalidOtpException extends RuntimeException {
-    public InvalidOtpException(String message) {
-        super(message);
-    }
-}
--- employee_management_system/src/main/java/com/employee_management_system/exception/OtpExpiredException.java
+++
@@ -1,7 +0,0 @@
-package com.employee_management_system.exception;
-
-public class OtpExpiredException extends RuntimeException {
-    public OtpExpiredException(String message) {
-        super(message);
-    }
-}
