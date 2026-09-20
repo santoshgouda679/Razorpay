b26f3e6130113b43a08f46bc240b7aba8e83d914
santoshgouda679 <santoshgoudag2@gmail.com>
2026-09-20 22:44:02 +0530

    Remove bank Management System files from this repo

--- bank_system/src/main/java/com/example/bank_system/repository/BankRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.bank_system.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.bank_system.entity.Bank;
-
-public interface BankRepository extends JpaRepository<Bank, Integer>{
-	
-}
